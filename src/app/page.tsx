a5cbd372fa68a646b093c7944dcc5cc3f5affe70
Jose Nunez <jcnv21@gmail.com>
2024-03-04 15:33:04 -0600

    remove lines

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,15 @@
-// import React from "react";
-
-// export default function Home() {
-//   return (
-//     <div className="min-h-screen">
-//       <h3>Bienvenido a QuiniSports</h3>
-//     </div>
-//   );
-// }
-
 // import CountUp from "react-countup";
 // import VisibilitySensor from "react-visibility-sensor";
 
 import HomeHeader from "@/components/_/headers/HomeHeader";
 import AboutSectionOne from "@/components/pages/home/About/AboutSectionOne";
-import AboutSectionTwo from "@/components/pages/home/About/AboutSectionTwo";
-import Blog from "@/components/pages/home/Blog";
 import Brands from "@/components/pages/home/Brands";
 import ScrollUp from "@/components/pages/home/Common/ScrollUp";
 import Contact from "@/components/pages/home/Contact";
 import Features from "@/components/pages/home/Features";
 import Hero from "@/components/pages/home/Hero";
-import Pricing from "@/components/pages/home/Pricing";
 import Testimonials from "@/components/pages/home/Testimonials";
 import Video from "@/components/pages/home/Video";
-import Image from "next/image";
-import Link from "next/link";
 
 export default function Main() {
   return (
