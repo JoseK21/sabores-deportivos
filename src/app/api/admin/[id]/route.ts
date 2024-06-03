657503261c54b4c45a003fda120d97aa3a4d7c5c
Jose Nunez <jcnv21@gmail.com>
2024-06-03 14:36:37 -0600

    update api admin_employee routes

--- src/app/api/admin/[id]/route.ts
+++ src/app/api/admin/[id]/route.ts
@@ -1,29 +1,14 @@
 import prisma from "@/lib/prisma";
 import { requestMiddleware } from "@/middlewares/requestMiddleware";
 
-import { pick } from "lodash";
-
 export const GET = requestMiddleware(async ({ params }) => {
-  const adminUser = await prisma.user.findFirst({
-    where: { id: params.id },
-  });
-
-  return adminUser; // pick(adminUser, ["id", "name", "role", "image", "email", "status"]);
+  return await prisma.user.findFirst({ where: { id: params.id } });
 });
 
 export const PUT = requestMiddleware(async ({ data, params }) => {
-  const adminUser = await prisma.user.update({
-    where: { id: params.id },
-    data,
-  });
-
-  return adminUser; // pick(adminUser, ["id", "name", "role", "image", "email", "status"]);
+  return await prisma.user.update({ where: { id: params.id }, data });
 });
 
 export const DELETE = requestMiddleware(async ({ params }) => {
-  const adminUser = await prisma.user.delete({
-    where: { id: params.id },
-  });
-
-  return adminUser; // pick(adminUser, ["id", "name", "role"]);
+  return await prisma.user.delete({ where: { id: params.id } });
 });
