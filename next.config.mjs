149154873764ab08dc81f456ca3fdc3e9f2f1a09
Jose Nunez <jcnv21@gmail.com>
2024-06-13 00:32:24 -0600

    update v2 - CORS

--- next.config.mjs
+++ next.config.mjs
@@ -40,18 +40,10 @@ const nextConfig = {
       {
         source: "/api/:path*",
         headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://www.quinisports.com",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://www.quinisports.com" },
+          { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "Accept, Content-Type, Authorization" },
         ],
       },
     ];
