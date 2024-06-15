f520d3ea13aa14705113d2cf840479f48ae700cd
Luis Madriz <luismadriz@Luiss-MacBook-Pro.local>
2024-06-15 15:54:29 -0600

    Create function for validate schedule

--- src/app/(empty)/layout.tsx
+++ src/app/(empty)/layout.tsx
@@ -1,4 +1,12 @@
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
+
+  if (headers().get("quini-access") === "true") {
+    redirect("/");
+  }
+  
   return (
     <section>
       <main className="w-full flex-1 space-y-4 p-8">{children}</main>
