97d6e3ff6d7581d30d3a4efae00aa9b732c0b7d8
Jose Nunez <jcnv21@gmail.com>
2024-06-19 01:38:36 -0600

    remove middleware

--- src/DEPRECATED-middleware.ts
+++ src/DEPRECATED-middleware.ts
@@ -2,5 +2,5 @@ export { default } from "next-auth/middleware";
 
 // specify the path regex to apply the middleware to
 export const config = {
-  matcher: "/api/:path*",
+  // matcher: "/api/:path*", // me genera error la hacer peticiones al api, pues pide credenciales
 };
